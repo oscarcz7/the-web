ce3c938ea0d4adf57495953fe305cf442329678f
oscar catota <ocatotazurita@gmail.com>
2022-10-14 00:56:56 -0500

    Feature: Loading component and data administration for the page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
